bba792bfc851e27dab923022ebace3f8345613ee
Arne Neumann <github@arne.cl>
2024-04-26 14:57:21 +0200

    web app: show scores and importances as tables

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,11 +1,39 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+
 import Leaf_gm_architecture_functions as gm
 
 
 @st.cache_data  # this caches the aggregated input data between runs
 def load_data():
     return pd.read_parquet('gm_dataset_Knauer_et_al_2022_aggregated.parquet')
+
+
+def dict_to_tables(results):
+    """
+    Helper function to convert CV_with_PFT_and_combination_of_interest()
+    result dictionary into individual dataframes.
+
+    Parameters
+    ----------
+    results : dict
+        The resulting predictability scores and importance of the given
+        traits calculated in CV_with_PFT_and_combination_of_interest().
+        
+    Returns 
+    -------
+    predictability_scores_df : pd.DataFrame
+        DataFrame with one column per predictability score
+    importances_df : pd.DataFrame
+        DataFrame with one column per importance value
+    """
+    # create predictability scores DataFrame by excluding 'importances'
+    predictability_scores_df = pd.DataFrame({k: [v] for k, v in results.items() if k != 'importances'})
+
+    importances_df = pd.DataFrame({'Value': results['importances']})
+    return predictability_scores_df, importances_df
+
 
 
 # initialize session state for aggregated data
@@ -37,13 +65,12 @@ st.write('Selected PFT group:', selected_pft_group, pft_description)
 # Perform analysis
 if st.button('Perform Analysis'):
     results = gm.CV_with_PFT_and_combination_of_interest(st.session_state['aggregated_df'], gm.PFTs[selected_pft_group], selected_traits, ensemble_size=50, min_rows=50)
-    
-    if isinstance(results, pd.DataFrame):
-        st.write(results)
-        results.to_csv('results.csv', index=False)
-        st.success('Analysis complete and results saved!')
-    elif isinstance(results, dict):
-        st.json(results)  # Display the results as JSON in the app
-        st.error('Analysis results are not in tabular form. See the JSON output.')
-    else:
-        st.error('Unexpected result type: ' + str(type(results)))
+
+    predictability_scores_df, importances_df = dict_to_tables(results)
+    # show predictability scores table
+    st.subheader('Predictability scores')
+    st.dataframe(predictability_scores_df)
+
+    # show importances table
+    st.subheader('Gini importances')
+    st.dataframe(importances_df)
