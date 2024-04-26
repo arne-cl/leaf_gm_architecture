ced3aec2d94e470159fa47b00ecb61484e86dfa3
Arne Neumann <github@arne.cl>
2024-04-26 11:15:40 +0200

    cleanup web app

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,18 +2,15 @@ import streamlit as st
 import pandas as pd
 import Leaf_gm_architecture_functions as gm
 
-# Load the dataset
-@st.cache_data
+
+@st.cache_data  # this caches the aggregated input data between runs
 def load_data():
     return pd.read_parquet('gm_dataset_Knauer_et_al_2022_aggregated.parquet')
 
-# ~ aggregated_df = load_data()
 
-# Initialize session state for aggregated data
+# initialize session state for aggregated data
 if 'aggregated_df' not in st.session_state:
     st.session_state['aggregated_df'] = load_data()
-
-# ~ combination_of_traits = ['T_cw','Sc','T_leaf', 'D_leaf']
 
 # Sidebar for parameter selection
 st.sidebar.header('Parameters')
@@ -23,35 +20,30 @@ selected_traits = st.sidebar.multiselect(
     default=['T_cw','Sc','T_leaf', 'D_leaf'])
 
 pft_group_options = list(gm.PFTs.keys())
-selected_pft = st.sidebar.selectbox(
+selected_pft_group = st.sidebar.selectbox(
     'Select PFT Group',
     options=pft_group_options,
     index=pft_group_options.index('global_set'))
 
-# Button to perform aggregation
-# ~ if st.button('Aggregate Data'):
-    # ~ st.session_state['aggregated_df'] = gm.data_aggregation(df)
-    # ~ st.write('Data Aggregated!')
-
 # Display the selected parameters
-st.write('Selected Traits:', selected_traits)
-st.write('Selected PFT:', selected_pft)
+st.write('Selected Traits:', ", ".join(selected_traits))
+
+pft_list = gm.PFTs[selected_pft_group]
+pft_description = ""
+if len(pft_list) > 1:
+    pft_description = f"(which contains these PFTs: {', '.join(gm.PFTs[selected_pft_group])})"
+st.write('Selected PFT group:', selected_pft_group, pft_description)
 
 # Perform analysis
 if st.button('Perform Analysis'):
-    if st.session_state['aggregated_df'] is not None:
-        results = gm.CV_with_PFT_and_combination_of_interest(st.session_state['aggregated_df'], gm.PFTs[selected_pft], selected_traits, ensemble_size=50, min_rows=50)
-        
-        if isinstance(results, pd.DataFrame):
-            st.write(results)
-            results.to_csv('results.csv', index=False)
-            st.success('Analysis complete and results saved!')
-        elif isinstance(results, dict):
-            st.json(results)  # Display the results as JSON in the app
-            st.error('Analysis results are not in tabular form. See the JSON output.')
-        else:
-            st.error('Unexpected result type: ' + str(type(results)))
+    results = gm.CV_with_PFT_and_combination_of_interest(st.session_state['aggregated_df'], gm.PFTs[selected_pft_group], selected_traits, ensemble_size=50, min_rows=50)
+    
+    if isinstance(results, pd.DataFrame):
+        st.write(results)
+        results.to_csv('results.csv', index=False)
+        st.success('Analysis complete and results saved!')
+    elif isinstance(results, dict):
+        st.json(results)  # Display the results as JSON in the app
+        st.error('Analysis results are not in tabular form. See the JSON output.')
     else:
-        st.error('Please aggregate the data first.')
-
-# To run this app, save this script as app.py and in your terminal run: streamlit run app.py
+        st.error('Unexpected result type: ' + str(type(results)))
