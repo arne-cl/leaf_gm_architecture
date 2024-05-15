c9825e2ab31c99776319d92334b77aac6ed9fced
Arne Neumann <github@arne.cl>
2024-05-15 17:57:44 +0200

    WIP: add caching

--- src/leaf_gm_architecture/streamlit_crosspred_global_pft.py
+++ src/leaf_gm_architecture/streamlit_crosspred_global_pft.py
@@ -6,11 +6,35 @@ import leaf_gm_architecture.functions as gm
 from leaf_gm_architecture.streamlit_utils import dict_to_tables, PageNames
 
 
+@st.cache_data
+def get_global_PFT_results(df_agg, PFT_of_interest, traits_list, ensemble_size, min_train_rows, min_test_rows):
+    return gm.cross_prediction_global_PFT(
+        df_agg=df_agg,
+        PFT_of_interest=PFT_of_interest,
+        traits_list=traits_list,
+        ensemble_size=ensemble_size,
+        minimum_train_rows=min_train_rows,
+        minimum_test_rows=min_test_rows
+    )
+
+@st.cache_data
+def get_global_PFT_comb_interest_results(df_agg, PFT_of_interest, combination_of_interest, ensemble_size, min_train_rows, min_test_rows):
+    return gm.cross_prediction_global_PFT_with_combination_of_interest(
+        df_agg=df_agg,
+        PFT_of_interest=PFT_of_interest,
+        combination_of_interest=combination_of_interest,
+        ensemble_size=ensemble_size,
+        minimum_train_rows=min_train_rows,
+        minimum_test_rows=min_test_rows
+    )
+
+
 def app():
     st.header(PageNames.CROSSPRED_GLOBAL_PFT.value)
     
     # Initialize session state for `selected_traits_detailed_analysis` if it doesn't exist
     if 'selected_traits_detailed_analysis' not in st.session_state:
+        print(f"selected_traits_detailed_analysis is empty!")
         st.session_state.selected_traits_detailed_analysis = []
 
     selected_traits = st.multiselect(
@@ -23,13 +47,13 @@ def app():
     min_test_rows = st.number_input('Minimum Test Rows', min_value=1, value=10, step=1)
 
     if st.button('Calculate Cross Prediction: global-PFT'):
-        global_PFT_results = gm.cross_prediction_global_PFT(
+        global_PFT_results = get_global_PFT_results(
             df_agg=st.session_state['aggregated_df'],
             PFT_of_interest=['ferns'],
             traits_list=selected_traits, 
             ensemble_size=ensemble_size,
-            minimum_train_rows=min_train_rows, 
-            minimum_test_rows=min_test_rows
+            min_train_rows=min_train_rows, 
+            min_test_rows=min_test_rows
         )
 
         if global_PFT_results.shape[0] > 0:
@@ -56,13 +80,13 @@ def app():
             st.session_state.selected_traits_detailed_analysis = selected_traits_detailed_analysis
 
             if selected_traits_detailed_analysis:
-                global_PFT_comb_interest_results = gm.cross_prediction_global_PFT_with_combination_of_interest(
+                global_PFT_comb_interest_results = get_global_PFT_comb_interest_results(
                     df_agg=st.session_state['aggregated_df'],
                     PFT_of_interest=['ferns'],
                     combination_of_interest=selected_traits_detailed_analysis, 
                     ensemble_size=ensemble_size,
-                    minimum_train_rows=min_train_rows, 
-                    minimum_test_rows=min_test_rows
+                    min_train_rows=min_train_rows, 
+                    min_test_rows=min_test_rows
                 )
 
                 st.header(PageNames.CROSSPRED_GLOBAL_PFT.value + " (with combination of interest)")
